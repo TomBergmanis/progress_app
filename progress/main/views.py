78a4062834fb7a26305598e786fe092a343a05e1
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2024-03-22 09:16:59 +0000

    users can now add goals and view their goals. other users cannot see other users goals

--- progress/main/views.py
+++ progress/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required 
 from .models import Homepage, Goal
 from .forms import GoalForm
